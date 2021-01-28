9c89dde2998a5b80b788cec2abf3b39749f20a73
matheuscosta <matheuscostajgppo@gmail.com>
2021-01-27 21:33:41 -0300

    add css

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,57 @@
+import { delBasePath } from "next/dist/next-server/lib/router/router";
 import styled from "styled-components";
+import db from "../db.json";
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+`;
 
-//function Title(props) {
-//  return (
-//  <h1>
-//    {props.children}
-//  </h1>
-//  )
-//}
+
+export const QuizContainer = styled.div`
+width: 100%;
+max-width: 350px;
+padding-top: 45px;
+margin: auto;
+@media screen and (max-width: 500px) {
+  margin:auto;
+  padding: 15px;
+  }
+}`
+
+const Widget = styled.div`
+  margin-top: 24px;
+  margin-bottom: 24px;
+  border: 1px solid #4CAF50;
+  background-color: #1C1814;
+  border-radius: 4px;
+  overflow: hidden;
+
+  h1, h2, h3 {
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 1;
+    margin-bottom: 0;
+  }
+  p {
+    font-size: 14px;
+    font-weight: 700;
+    line-height: 1;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>;
+
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        dasdsaasdd
+      </QuizContainer>
+    </BackgroundImage>
+
+
+
+  );
 }
