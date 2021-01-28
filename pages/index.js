5957b709f2b8d950163bdeb168f3ddff5bd019df
matheuscosta <matheuscostajgppo@gmail.com>
2021-01-27 22:06:43 -0300

    create folder src/component/Widget

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { delBasePath } from "next/dist/next-server/lib/router/router";
 import styled from "styled-components";
 import db from "../db.json";
+import Widget from '../src/components/Widget'
 
 const BackgroundImage = styled.div`
   background-image: url(${db.bg});
@@ -20,64 +21,24 @@ margin: auto;
   }
 }`;
 
-const Widget = styled.div`
-  margin-top: 24px;
-  margin-bottom: 24px;
-  border: 1px solid #4caf50;
-  background-color: #1c1814;
-  border-radius: 4px;
-  overflow: hidden;
-
-  h1,
-  h2,
-  h3 {
-    font-size: 16px;
-    font-weight: 700;
-    line-height: 1;
-    margin-bottom: 0;
-  }
-  p {
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 1;
-  }
-`;
-Widget.Header = styled.header` 
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  padding: 18px 32px;
-  background-color: ${({ theme })=> theme.colors.primary}; 
-
-
-`
-Widget.Content = styled.div`
-  padding: 24px 32px 32px 32px;
-  & > *:first-child {
-    margin-top: 0;
-  }
-  & > *:last-child {
-    margin-bottom: 0;
-  }
-  ul {
-    list-style: none;
-    padding: 0;
-  }
-`;
 
 export default function Home() {
   return (
     <BackgroundImage>
       <QuizContainer>
         <Widget>
-          <Widget.Content>
+          <Widget.Header>
             <h1>Localiza - Quizz</h1>
+          </Widget.Header>
+          <Widget.Content>
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
         <Widget>
-          <Widget.Content>
+          <Widget.Header>
             <h1>Localiza - Quizz</h1>
+          </Widget.Header>
+          <Widget.Content>
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
