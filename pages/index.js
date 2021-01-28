26fa3635a159c7427d02a6721251fd5f377462af
matheuscosta <matheuscostajgppo@gmail.com>
2021-01-27 21:52:29 -0300

    add new bg and css

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,6 @@ const BackgroundImage = styled.div`
   background-position: center;
 `;
 
-
 export const QuizContainer = styled.div`
 width: 100%;
 max-width: 350px;
@@ -19,17 +18,19 @@ margin: auto;
   margin:auto;
   padding: 15px;
   }
-}`
+}`;
 
 const Widget = styled.div`
   margin-top: 24px;
   margin-bottom: 24px;
-  border: 1px solid #4CAF50;
-  background-color: #1C1814;
+  border: 1px solid #4caf50;
+  background-color: #1c1814;
   border-radius: 4px;
   overflow: hidden;
 
-  h1, h2, h3 {
+  h1,
+  h2,
+  h3 {
     font-size: 16px;
     font-weight: 700;
     line-height: 1;
@@ -41,17 +42,46 @@ const Widget = styled.div`
     line-height: 1;
   }
 `;
+Widget.Header = styled.header` 
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  padding: 18px 32px;
+  background-color: ${({ theme })=> theme.colors.primary}; 
+
+
+`
+Widget.Content = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
 
 export default function Home() {
-
   return (
     <BackgroundImage>
       <QuizContainer>
-        dasdsaasdd
+        <Widget>
+          <Widget.Content>
+            <h1>Localiza - Quizz</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Content>
+            <h1>Localiza - Quizz</h1>
+            <p>lorem ipsum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
       </QuizContainer>
     </BackgroundImage>
-
-
-
   );
 }
