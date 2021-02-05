2ca7a7500dd366894f6b8337cc103e1f5fccd8ad
matheuscosta <matheuscostajgppo@gmail.com>
2021-02-04 23:23:56 -0300

    Quiz Page

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import { createGlobalStyle, ThemeProvider } from 'styled-components';
@@ -28,8 +29,9 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-const theme = db.theme;
+const { theme } = db;
 
+// eslint-disable-next-line react/prop-types
 export default function App({ Component, pageProps }) {
   return (
     <>
