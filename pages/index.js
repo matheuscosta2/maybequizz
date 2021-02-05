9f39ed5c2653f8ae5aa82c9e72340331de078f5e
matheuscosta <matheuscostajgppo@gmail.com>
2021-02-04 23:03:44 -0300

    eslint

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,30 @@
 import { delBasePath } from "next/dist/next-server/lib/router/router";
 import styled from "styled-components";
-import db from '../db.json';
-import Widget from '../src/components/Widget'
-import QuizBackground from '../src/components/QuizBackground'
-import Footer from '../src/components/Footer'
-import GitHubCorner from '../src/components/GitHubCorner'
+import Head from "next/head";
 
+import db from "../db.json";
+import Widget from "../src/components/Widget";
+import QuizBackground from "../src/components/QuizBackground";
+import Footer from "../src/components/Footer";
+import GitHubCorner from "../src/components/GitHubCorner";
 
-//const BackgroundImage = styled.div`
-//  background-image: url(${db.bg});
-//  flex: 1;
-//  background-size: cover;
-//  background-position: center;
-//`;
-
-export const QuizContainer = styled.div`
+const QuizContainer = styled.div`
 width: 100%;
 max-width: 350px;
 padding-top: 45px;
-margin: auto;
+margin: auto 10%;
 @media screen and (max-width: 500px) {
   margin:auto;
   padding: 15px;
   }
 }`;
 
-
 export default function Home() {
   return (
-    <QuizBackground backgroundImage ={db.bg}>
+    <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title> Localiza Quiz </title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
@@ -46,9 +42,9 @@ export default function Home() {
             <p>lorem ipsum dolor sit amet...</p>
           </Widget.Content>
         </Widget>
-        <Footer/>
+        <Footer />
       </QuizContainer>
-      <GitHubCorner projectUrl = "https://github.com/matheuscosta2"/>
+      <GitHubCorner projectUrl="https://github.com/matheuscosta2" />
     </QuizBackground>
   );
 }
